dabf4c4202a336ce30f30caf367aeabeba0e5145
unknown <fizz.zehra14@gmail.com>
2021-08-02 19:53:54 -0500

    Category model created

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,27 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:
+    {
+      type:Datatypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    product_name:
+    {
+      type:Datatypes.STRING,
+      allowNull:false
+    },
+    price:
+    {
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal:true
+      }
+
+    }
   },
   {
     sequelize,
