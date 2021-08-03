f9a0fcd57ea8695347e4ea87877502acee107786
unknown <fizz.zehra14@gmail.com>
2021-08-02 19:41:22 -0500

    adding varables

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+id:{
+  type: DataTypes.INTEGER,
+  allowNull:false,
+  primarykey:true,
+  autoIncrement:true
+
+},
+category_name:{
+  type: Datatypes.STRING,
+  allowNull:false
+}
   },
   {
     sequelize,
