da19aff2bb7a9156ffa3d5ba8f009f7538332fbc
unknown <fizz.zehra14@gmail.com>
2021-08-04 23:11:33 -0500

    Relations sucessfully created!

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -12,40 +12,40 @@ Product.init(
     // define columns
     id:
     {
-      type:Datatypes.INTEGER,
-      allowNull:false,
-      primaryKey:true,
-      autoIncrement:true
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     product_name:
     {
-      type:Datatypes.STRING,
-      allowNull:false
+      type: Datatypes.STRING,
+      allowNull: false
     },
     price:
     {
-      type:DataTypes.DECIMAL,
-      allowNull:false,
-      validate:{
-        isDecimal:true
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
       }
     },
     stock:
     {
-      type:Datatypes.INTEGER,
-    allowNull:false,
-    defaultValue:10,
-    validate:
-    {
-      isNumeric:true
-    }
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate:
+      {
+        isNumeric: true
+      }
     },
     category_id:
     {
-      type:Datatypes.INTEGER,
-      references:{
-        model:'Category',
-        key:'id'
+      type: Datatypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
       }
     }
   },
