2b9ec3cfb1804bf4a0d6095e5357e1fac5bf93ad
unknown <fizz.zehra14@gmail.com>
2021-08-06 01:18:46 -0500

    tags routes all set! tested

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     include:[
       {
         model:Product,
-        attributes: ['product_name', 'price', 'stock'],
+        attributes: ['product_name', 'price', 'stock']
        // through: ProductTag,
        // as: "products",
       }
@@ -25,19 +25,84 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where:
+    {
+      id:req.params.id
+    },
+    // be sure to include its associated Product data
+    include:[
+      {
+        model:Product,
+        attributes: ['product_name', 'price', 'stock']
+       // through: ProductTag,
+       // as: "products",
+      }
+    ]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name:req.body.tag_name
+  })
+  .then(tagData=>res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },
+    {
+      where:
+      {
+        id: req.params.id
+      }
+    })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No Tag is found with this id' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Category.destroy({
+    where:
+    {
+      id:req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag is found with this id' });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+
 });
 
 module.exports = router;
